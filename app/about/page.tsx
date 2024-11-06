b66f680743e19c4a66e8f091a1ff7e2831021ddd
RamisaFatima2005 <163189004+RamisaFatima2005@users.noreply.github.com>
2024-11-06 19:23:22 +0500

    Skincare_Website_Customcss

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className='bg-gradient'>
+            <div className='container-padding'>
+                <h2 className='font-extrabold'>About Our Brand</h2>
+                <p>We believe in natural beauty and sustainable skincare products.</p>
+            </div>
+        </div>
+    );
+};
+
+export default About;
